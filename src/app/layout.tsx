16776223beddca5ab5ca4da0621625614dcbfa8d
Lakshay Maini <62194911+lakshh07@users.noreply.github.com>
2024-03-22 13:43:33 +0530

    added google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import { Toaster } from "@/components/ui/sonner";
 import "@/styles/globals.css";
+import Script from "next/script";
 
 const poppins = Poppins({
   weight: ["400", "500", "600"],
@@ -22,6 +23,21 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={poppins.className}>
+        <Script
+          strategy="lazyOnload"
+          src={`https://www.googletagmanager.com/gtag/js?id=G-DR5DB9Q2BG`}
+        />
+
+        <Script id="google-analytics-script" strategy="lazyOnload">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-DR5DB9Q2BG', {
+            page_path: window.location.pathname,
+            });
+          `}
+        </Script>
         {children}
         <Toaster />
       </body>
