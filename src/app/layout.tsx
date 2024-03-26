e0668b97b2c63988ca00192981048654dfa3587d
Lakshay Maini <62194911+lakshh07@users.noreply.github.com>
2024-03-26 12:35:04 +0530

    added metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,8 +11,32 @@ const poppins = Poppins({
 });
 
 export const metadata: Metadata = {
+  robots: "index, follow",
   title: "Quick Signature",
   description: "Easily Create Your Signature in PNG Format Instantly",
+  applicationName: "Quick Signature",
+  authors: [{ name: "Lakshay Maini", url: "https://lakshaymaini.com" }],
+  keywords: ["nextjs, tailwindcss, shadcn-ui, signature, quick sign, sign"],
+  openGraph: {
+    type: "website",
+    url: "https://quicksign.vercel.app/",
+    title: "Quick Signature",
+    description: "Easily Create Your Signature in PNG Format Instantly",
+    siteName: "Quick Signature",
+    images: [
+      {
+        url: "https://i.postimg.cc/4NvGZjPV/Screenshot-2024-03-26-at-12-31-12-PM.png",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    creator: "@LakshayMaini_",
+    images:
+      "https://i.postimg.cc/4NvGZjPV/Screenshot-2024-03-26-at-12-31-12-PM.png",
+    title: "Quick Signature",
+    description: "Easily Create Your Signature in PNG Format Instantly",
+  },
 };
 
 export default function RootLayout({
